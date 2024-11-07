9c54e455f435f0e40c8bbf5a8e4e6f6c82802a25
Darshan Thakkar <95502334+mrgenius17@users.noreply.github.com>
2024-11-07 18:57:10 -0500

    Update pagetable.c

--- pagetable.c
+++ pagetable.c
@@ -7,3 +7,6 @@ int main() {
 
   return 0;
 }
+
+// be careful when accessing the array, in for loops use 1 to 7 instead of 0 to 7.
+// when you get a virtual address from the mempart2.c, look up the physical table 
